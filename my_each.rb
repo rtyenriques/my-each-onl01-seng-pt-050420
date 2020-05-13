1bf9794f2e661ed365513e502802a5467aff76fe
harmonious-cable-1188 <rtyenriques@gmail.com>
2020-05-13 02:51:13 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -5,3 +5,11 @@ def my_each(array)
     i += 1
     end
 end
+
+colors = ["Gray", "Blue", "Green", ]
+
+my_each (colors) do |color|
+  if color.start_with ? ("G")
+    puts "Colors thats start with G are #{color}"
+  end
+end
